ac4b70e7d4a03feb493624d2d9e78169c9d59867
Gohstman2 <gohst.man05@gmail.com>
2025-06-28 18:52:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from flask import Flask
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto("https://example.com")
-    print(page.title())
-    browser.close()
+app = Flask(__name__)
+
+@app.route("/")
+def test_playwright():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto("https://example.com")
+        title = page.title()
+        browser.close()
+        return f"Titre de la page : {title}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
